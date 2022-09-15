cb0b789b29b492f3f151f7f5f0204f0eb70be820
jayaprakashajaishee <jayaprakashajaishee@gmail.com>
2022-09-15 12:01:32 +0530

    Responsive SourceDrawer Button added

--- news/src/component/Navbar/Navbar.js
+++ news/src/component/Navbar/Navbar.js
@@ -4,13 +4,19 @@ import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import { themeChange } from "../action/action";
 import IconButton from "@mui/material/IconButton";
+import SourceDrawer from "../Sources/SourceDrawer";
 
 function Navbar() {
   const theme = useSelector((state) => state.theme);
   const dispatch = useDispatch();
   return (
     <div className="navbar">
-      <h2 style={{ fontWeight: 400, marginLeft: "20px" }}>News Feed</h2>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <SourceDrawer />
+        <h2 style={{ fontWeight: 400, marginLeft: "20px" }}>
+          News Feed
+        </h2>
+      </div>
       <IconButton
         onClick={() => dispatch(themeChange())}
         variant="contained"
--- news/src/component/Sources/SourceDrawer.js
+++ news/src/component/Sources/SourceDrawer.js
@@ -1,18 +1,51 @@
-import React from "react";
-import Box from "@mui/material/Box";
-import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import React, { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import Sources from "./Sources";
+import FavouriteSources from "../FavouriteSources/FavouriteSources";
+import { useSelector } from "react-redux";
+import {
+  IconButton,
+  Box,
+  SwipeableDrawer,
+  List,
+  Divider,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Drawer,
+} from "@mui/material";
 
 function SourceDrawer() {
-  return <div>SourceDrawer</div>;
+  const [drawer, setDrawer] = useState(false);
+  const handleDrawer = () => setDrawer((prev) => !prev);
+  const theme = useSelector((state) => state.theme);
+  return (
+    <div style={{ marginLeft: "20px" }} className="sourceDrawerButton">
+      <IconButton onClick={handleDrawer}>
+        <MenuIcon />
+      </IconButton>
+      <Drawer
+        anchor="left"
+        open={drawer}
+        onClose={handleDrawer}
+        className={theme}
+      >
+        <div
+          style={{
+            width: "80vw",
+            height: "100vh",
+            overflow: "auto",
+          }}
+          className={`${theme}`}
+        >
+          <Sources />
+          <FavouriteSources />
+        </div>
+      </Drawer>
+    </div>
+  );
 }
 
 export default SourceDrawer;
